dae9859976dd732e6b531686651bb9258c2c3df6
luosichen <97505288+luosichen@users.noreply.github.com>
2024-07-14 11:49:42 +0800

    feat：代码提交

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
+  buttonYes: "Tớ đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn cậu.",
+  descriptionModar:
+    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+};
